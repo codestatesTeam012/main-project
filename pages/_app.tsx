198bca5dd94c795c65124d2e663239d2eec708ae
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-13 16:18:27 +0900

    feat : firstProject Setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 import type {AppProps} from 'next/app'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {RecoilRoot} from 'recoil'
 import {GlobalStyle} from '../styles/GlobalStyle'
 
-const queryClient = new QueryClient()
-
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </RecoilRoot>
   )
 }
 
